d53be50c6c1c7bb0797a2f6250e73396826df77b
padakrishnakp <padakrishnakp@gmail.com>
2024-10-21 11:28:38 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const connectDB = require('./config/database');
+const userRoutes = require('./routes/userRoutes');
+require('dotenv').config();
+
+const app = express();
+
+// Connect to the database
+connectDB();
+
+// Middleware
+app.use(express.json()); // To parse incoming JSON data
+
+// Use the user routes with '/api' prefix
+app.use('/api', userRoutes);  // This makes '/api/users' available
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Backend/controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require('../models/user');
+
+const getUsers = async (req, res) => {
+  try {
+    console.log("LIST")
+
+    return {errorCode:1,message:"List"}
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const addUser = async (req, res) => {
+    console.log("*****")
+  const { name, email, age } = req.body;
+  try {
+    const newUser = new User({ name, email, age });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { getUsers, addUser };
